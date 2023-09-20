8da277c151a4f69cd16cfd2e50e2d17cfd84dbcc
Fazil <122675114+fazilcodes@users.noreply.github.com>
2023-09-20 23:44:53 +0530

    Initial Portfolio Setup

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Navbar from './components/Navbar'
+
+function App() {
+
+  return (
+    <>
+      <div className='App'>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </Router>
+      </div>
+    </>
+  )
+}
+
+export default App
